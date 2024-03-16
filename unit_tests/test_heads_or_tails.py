0426d228a8348a8766d2f32a06242a626e532429
wpnx777 <wpnx777@wpnx777.com>
2024-03-16 07:46:51 -0400

    Added Timer Stats

--- unit_tests/test_heads_or_tails.py
+++ unit_tests/test_heads_or_tails.py
@@ -2,6 +2,7 @@ from heads_or_tails.game_engine.heads_or_tails import HeadsOrTails
 
 def main() -> None:
     num_flips = input("\nEnter The Number Of Coin Flips: ")
+    print("")
 
     heads_or_tails = HeadsOrTails(num_flips)
 
